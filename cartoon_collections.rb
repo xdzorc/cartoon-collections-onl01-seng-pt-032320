fff96d170798c70a04e0680c32a7021e8710acf5
DAZUN XING <dazun.xing@gmail.com>
2020-04-02 04:58:30 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(name)# code an argument here
+  name.each_with_index do|a,b|
+    puts "#{b+1}.*#{a}"
+  end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(name)# code an argument here
+  name.collect do |x|
+    "#{x.capitalize}!"
+  end
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(name)# code an argument here
+  name.any?{|x| x.length>4}
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(name)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  name.find {|x| cheese_types.include?(x)}
+  
 end
