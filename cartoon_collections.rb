8d1122bb2fce0e9a2d054f2b6ec13eaf5423e6ec
DAZUN XING <dazun.xing@gmail.com>
2020-04-02 04:41:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(name)# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(name)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
